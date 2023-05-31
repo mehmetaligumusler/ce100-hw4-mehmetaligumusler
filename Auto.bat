b5b50f5d0a266533e8b6afd223b1fc43e399381b
gokaydervisoglu <gokaydervisoglu@gmail.com>
2023-05-31 22:49:28 +0300

    First Commit.

--- Auto.bat
+++ Auto.bat
@@ -1,16 +1,16 @@
 @echo off
 
 echo Which branch would you like to switch to?
-echo 1. feat
-echo 2. master
+echo 1. main
+echo 2. feat
 echo 3. flow
 
 set /p branch_choice=Enter your branch selection (1/2/3): 
 
 IF %branch_choice%==1 (
-    set branch_name=feat
+    set branch_name=main
 ) ELSE IF %branch_choice%==2 (
-    set branch_name=master
+    set branch_name=feat
 ) ELSE IF %branch_choice%==3 (
     set branch_name=flow
 ) ELSE (
