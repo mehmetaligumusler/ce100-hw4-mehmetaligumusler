4a91479c7ef18f66d0c48160d993d26b36bc6f9c
gokaydervisoglu <gokaydervisoglu@gmail.com>
2023-05-31 23:01:30 +0300

    Some Problems Fixed

--- Auto.bat
+++ Auto.bat
@@ -1,5 +1,6 @@
 @echo off
 
+cls
 echo Which branch would you like to switch to?
 echo 1. main
 echo 2. feat
@@ -18,12 +19,42 @@ IF %branch_choice%==1 (
     exit /b
 )
 
-set /p commit_message=Enter your commit message: 
+cls
+:enter_commit_message
+echo Enter your commit message:
+echo 1. Some Problems Fixed
+echo 2. Custom message
 
+set /p commit_choice=Enter your commit message selection (1/2): 
+
+IF %commit_choice%==1 (
+    set commit_message=Some Problems Fixed
+) ELSE IF %commit_choice%==2 (
+    set /p commit_message=Enter your custom commit message: 
+) ELSE (
+    echo Invalid input. Please enter a valid option.
+    goto enter_commit_message
+)
+
+cls
+echo Confirm your commit message: %commit_message%
+set /p confirm_commit=Is this correct? (Y/N): 
+
+IF /i "%confirm_commit%"=="Y" (
+    goto continue
+) ELSE IF /i "%confirm_commit%"=="N" (
+    goto enter_commit_message
+) ELSE (
+    echo Invalid input. Please enter Y for Yes or N for No.
+    goto enter_commit_message
+)
+
+:continue
 "C:\Program Files\Git\bin\bash.exe" -c "git switch %branch_name%"
 "C:\Program Files\Git\bin\bash.exe" -c "git add ."
 "C:\Program Files\Git\bin\bash.exe" -c "git commit -m ""%commit_message%"""
 "C:\Program Files\Git\bin\bash.exe" -c "git push -u origin %branch_name%"
 
+cls
 echo Finish Push!!!
 pause
